39e42f883580d1f3416468a77f84eaed640ac423
daffodilho <46694934+daffodilho@users.noreply.github.com>
2019-05-28 15:35:40 -0400

    basic JS review

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 (()=>{
     //stub
-    console.log(fired!);
+    console.log("fired!");
     
-    let
+    let headline = document.querySelector("h1"),
+        subhead = document.querySelector("h3");
+        theButton = document.querySelector("button")
     
+    //functions in the middle (what do you want to have happen)
     function changeText(){
-        
+        headline.textContent = "Now I'm different!";
+        subhead.textContent = "hey me too. but that's ok."
     }
     
-    changeText();
+    //function calls / event handling at the bottom
+    
+    theButton.addEventListener("click", changeText);
 })();
